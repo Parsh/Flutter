74ffbefdb023adc32b8eb182ecd0695a0b1b423d
Parsh-Dev <parsh.cosmos11@gmail.com>
2018-03-05 22:23:50 +0530

    Layout Created

--- tab_nav/lib/main.dart
+++ tab_nav/lib/main.dart
@@ -1,1 +1,53 @@
 import 'package:flutter/material.dart';
+import './FirstPage.dart' as First;
+import './SecondPage.dart' as Second;
+import './ThirdPage.dart' as Third;
+
+void main(){
+  runApp(
+    new MaterialApp(
+      home: new MyTabs()
+    )
+  );
+}
+
+class MyTabs extends StatefulWidget{
+  @override
+  createState() => new MyTabsState();
+}
+
+class MyTabsState extends State<MyTabs> with SingleTickerProviderStateMixin {
+
+  TabController controller;
+
+  @override
+  void initState(){
+    super.initState();
+    controller = new TabController(vsync: this, length: 3);
+  }
+
+  @override
+  void dispose(){
+    controller.dispose();
+    super.dispose();
+  }
+
+
+  @override
+  Widget build(BuildContext context){
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Tab Navigation'),
+        backgroundColor: Colors.deepOrange,
+        bottom: new TabBar(
+          controller: controller,
+          tabs: <Tab>[
+            new Tab(icon: new Icon(Icons.arrow_forward)),
+            new Tab(icon: new Icon(Icons.arrow_downward)),
+            new Tab(icon: new Icon(Icons.arrow_back))
+          ],
+        ),
+      ),
+    );
+  }
+}
