1a1df65c14f2fd8798992adbec8b3ad25c313b06
Parsh-Dev <parsh.cosmos11@gmail.com>
2018-03-05 10:21:16 +0530

    Concluded Stateful Widget App

--- stateful/lib/main.dart
+++ stateful/lib/main.dart
@@ -10,6 +10,17 @@ class AwesomeButton extends StatefulWidget{
 
 class AwesomeButtonState extends State<AwesomeButton>{
 
+  int counter = 0;
+  List<String> strings = ["This is", "going", "to be", "Awesome"];
+  String displayedString = "";
+
+  void onPress(){
+    setState((){
+      displayedString = strings[counter];
+      counter = counter < strings.length - 1 ? counter + 1 : 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context){
     return new Scaffold(
@@ -19,11 +30,12 @@ class AwesomeButtonState extends State<AwesomeButton>{
           child: new Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              new Text("Can you dig it"),
+              new Text(displayedString, style: new TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold)),
+              new Padding(padding: new EdgeInsets.all(20.0)),
               new RaisedButton(
-                child: new Text("Press me"),
+                child: new Text("Press me", style: new TextStyle(color: Colors.white, fontStyle: FontStyle.italic, fontSize: 20.0 )),
                 color: Colors.deepOrange,
-                onPressed: null
+                onPressed: onPress
               )
             ],)
         )
