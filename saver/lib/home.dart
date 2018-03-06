b6d427afea54915a154d7f4b9e64b8e6092085e5
Parsh-Dev <parsh.cosmos11@gmail.com>
2018-03-06 22:00:04 +0530

    Version 1.0

--- saver/lib/home.dart
+++ saver/lib/home.dart
@@ -1,4 +1,8 @@
+import "dart:io";
+import "dart:convert";
+
 import "package:flutter/material.dart";
+import "package:path_provider/path_provider.dart";
 
 class Home extends StatefulWidget{
   @override
@@ -7,20 +11,62 @@ class Home extends StatefulWidget{
 
 class HomeState extends State<Home>{
 
-  TextEditingController item_controller;
-  TextEditingController quantity_controller;
+  File jsonFile;
+  Directory dir;
+  String fileName = "ShoppingList.json";
+  bool fileExists = false;
+  Map<String, int> fileContent;
+
+  void createFile(Map<String, int> content, Directory dir, String fileName){
+    print("Creating File");
+    File file = new File(dir.path + '/' + fileName);
+    file.createSync();
+    fileExists = true;
+    file.writeAsStringSync(JSON.encode(content));
+    print('Wrote!')
+  }
+
+  void writeToFile(String item, int quantity){
+    print('Writing to File!');
+    Map<String, int> content = {item: quantity};
+    if (fileExists){
+      print('File Exists');
+      Map<String, int> jsonFileContent = JSON.decode(jsonFile.readAsStringSync());
+      jsonFileContent.addAll(content);
+      jsonFile.writeAsStringSync(JSON.encode(jsonFileContent));
+      setState(()=> fileContent = jsonFileContent);
+    }
+    else{
+      print('File does not exits');
+      createFile(content, dir, fileName);
+    }
+  }
+
+  TextEditingController itemController;
+  TextEditingController quantityController;
 
   @override
   void initState(){
     super.initState();
-    item_controller = new TextEditingController();
-    quantity_controller = new TextEditingController();
+    itemController = new TextEditingController();
+    quantityController = new TextEditingController();
+    
+    getApplicationDocumentsDirectory().then((Directory directory){
+      dir = directory;
+      jsonFile = new File(dir.path + '/' + this.fileName);
+      jsonFile.exists().then((bool val){
+        this.fileExists = val;
+        return this.fileExists;
+      }).then((bool fileEx){
+        if (fileEx) this.setState(()=> this.fileContent = JSON.decode(jsonFile.readAsStringSync()));
+      });
+    });
   }
 
   @override
   void dispose(){
-    item_controller.dispose();
-    quantity_controller.dispose();
+    itemController.dispose();
+    quantityController.dispose();
     super.dispose();
   }
 
@@ -36,22 +82,23 @@ class HomeState extends State<Home>{
           child: new Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              new Text("Let's Remember", style: new TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold, color: Colors.blue)),
+              //new Text("Let's Remember", style: new TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold, color: Colors.blue)),
+              new Text(fileContent.toString(), style: new TextStyle(fontWeight: FontWeight.bold, color: Colors.blue)),              
               new Padding(padding: new EdgeInsets.only(bottom: 20.0)),
               new TextField(
                 decoration: new InputDecoration(hintText: "Enter the Item to Buy"),
-                controller: item_controller, 
+                controller: itemController, 
               ),
               new Padding(padding: new EdgeInsets.only(bottom: 20.0)),
               new TextField(
                 decoration: new InputDecoration(hintText: "Enter the Quantity"),
-                controller: quantity_controller,
+                controller: quantityController,
               ),
               new Padding(padding: new EdgeInsets.only(bottom: 30.0)),
               new RaisedButton(
                 color: Colors.blue,
                 child: new Text("Save", style: new TextStyle(fontSize: 15.0, color: Colors.white)),
-                onPressed: null,
+                onPressed: () => writeToFile(itemController.text, int.parse(quantityController.text)),
               ),
               new Padding(padding: new EdgeInsets.only(bottom: 20.0)),
               new RaisedButton(
