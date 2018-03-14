56c2a19200e2d4361e84c1f0b9e04579b27e4dbc
Parsh-Dev <parsh.cosmos11@gmail.com>
2018-03-14 22:41:10 +0530

    Detail Section added and INKwell installed

--- cards/lib/myCard.dart
+++ cards/lib/myCard.dart
@@ -7,15 +7,11 @@ class MyCard extends StatefulWidget {
 
 class _MyCardState extends State<MyCard> {
 
-  @override
-  Widget build(BuildContext context) {
-    return new Container(
-          margin: new EdgeInsets.fromLTRB(8.0, 20.0, 8.0, 8.0),
-          child: new Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              new Image.network("https://images.news18.com/ibnlive/uploads/2017/03/Stephen-Hawking.jpg"),
-              new Container(
+
+  Widget briefSection(){
+    return new InkWell(
+      onTap: () => print("Tapped"),
+      child: new Container(
                 margin: new EdgeInsets.all(16.0),
                 child: new Row(
                   children: <Widget>[
@@ -35,8 +31,43 @@ class _MyCardState extends State<MyCard> {
                     )
                 ],)
               )
+    );
+  }
+
+  Widget detailSection(){
+
+    return new Column(
+      children: <Widget>[
+        new Divider(),
+        new Container(
+            margin: new EdgeInsets.all(16.0),
+            child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+                new Text("Physics can take one beyond one's physical limitations", style: Theme.of(context).textTheme.headline),
+                new Padding(padding: new EdgeInsets.only(bottom: 10.0)),
+                new Text("One of the Greatest Minds of All Time, who set the theory of cosmology as the union of relativity and quantum mechanics. Professor Stephen Hawking, #RIP", style: Theme.of(context).textTheme.body1)
+        ]
+      ),
+    )
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+          margin: new EdgeInsets.fromLTRB(8.0, 20.0, 8.0, 8.0),
+          child: new Card(
+            child: new Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              new Image.network("https://images.news18.com/ibnlive/uploads/2017/03/Stephen-Hawking.jpg"),
+              briefSection(),
+              detailSection()
             ],
           )
+         )     
       );
   }
 }
