044b4df1cd4145302ca9b7c3771b3e992c842104
Parsh-Dev <parsh.cosmos11@gmail.com>
2018-03-14 18:02:00 +0530

    Refactored 1

--- cards/lib/main.dart
+++ cards/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './myCard.dart' as card;
 
 void main() => runApp(new MyApp());
 
@@ -25,48 +26,7 @@ class HomePage extends StatelessWidget {
         title: new Text(title),
         backgroundColor: Colors.pink,
       ),
-      body: new MyCard()
+      body: new card.MyCard()
     );
-  }
-}
-
-class MyCard extends StatefulWidget {
-  @override
-  _MyCardState createState() => new _MyCardState();
-}
-
-class _MyCardState extends State<MyCard> {
-
-  @override
-  Widget build(BuildContext context) {
-    return new Container(
-          margin: new EdgeInsets.fromLTRB(8.0, 20.0, 8.0, 8.0),
-          child: new Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              new Image.network("https://images.news18.com/ibnlive/uploads/2017/03/Stephen-Hawking.jpg"),
-              new Container(
-                margin: new EdgeInsets.all(16.0),
-                child: new Row(
-                  children: <Widget>[
-                   new Expanded(
-                     child: new Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        new Text("Stephen Hawking", style: Theme.of(context).textTheme.headline),
-                        new Text("RIP 1942-2018", style: Theme.of(context).textTheme.body1)
-                      ],
-                    ),
-                   ),
-                    new FlatButton(
-                      color: Colors.pink,
-                      child: new Icon(Icons.favorite, color: Colors.white,),
-                      onPressed: () => print("Pressed"),
-                    )
-                ],)
-              )
-            ],
-          )
-      );
   }
 }
