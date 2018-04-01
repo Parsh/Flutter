4c9e7576f1e30f3daf27a300bdcd4bfbd4cc6b40
Parsh-Dev <parsh.cosmos11@gmail.com>
2018-04-01 20:19:13 +0530

    Cached Image Implemented

--- cookbook/lib/imageHome.dart
+++ cookbook/lib/imageHome.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:transparent_image/transparent_image.dart' as ti;
+import 'package:cached_network_image/cached_network_image.dart';
 
 class ImageHome extends StatefulWidget {
   @override
@@ -13,17 +13,23 @@ class _ImageHomeState extends State<ImageHome> {
       appBar: new AppBar(
         title: new Text("ImageHome"),
       ),
-      body: new Stack(
-        children: <Widget>[
-          new Center(child: new CircularProgressIndicator()),
-          new Center(
-            child: new FadeInImage.memoryNetwork(
-              placeholder: ti.kTransparentImage,
-              image:
-                  'https://github.com/flutter/website/blob/master/_includes/code/layout/lakes/images/lake.jpg?raw=true',
-            ),
-          ),
-        ],
+      // body: new Stack(
+      //   children: <Widget>[
+      //     new Center(child: new CircularProgressIndicator()),
+      //     new Center(
+      //       child: new FadeInImage.memoryNetwork(
+      //         placeholder: ti.kTransparentImage,
+      //         image:
+      //             'https://github.com/flutter/website/blob/master/_includes/code/layout/lakes/images/lake.jpg?raw=true',
+      //       ),
+      //     ),
+      //   ],
+      // ),
+      body: new Center(
+        child: new CachedNetworkImage(
+          placeholder: new CircularProgressIndicator(),
+          imageUrl: 'https://github.com/flutter/website/blob/master/_includes/code/layout/lakes/images/lake.jpg?raw=true',
+        ),
       ),
     );
   }
